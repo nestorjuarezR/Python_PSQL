941fb154b116daa94daddbc9f7ac95bce3a4387e
Néstor JR <74116503+nestorjuarezR@users.noreply.github.com>
2020-12-22 15:59:47 -0600

    Add files via upload
    Simulación básica de sistema con conexión a base de datos de una pastelería

--- data_cakes.py
+++ data_cakes.py
@@ -1,3 +1,5 @@
+# Create by Néstor Juárez
+
 import psycopg2
 
 TABLE_CAKE = ''' CREATE TABLE pasteles(
@@ -8,6 +10,19 @@ TABLE_CAKE = ''' CREATE TABLE pasteles(
     color VARCHAR(80)
     )'''
 
+def presentation():
+    print('''
+    <---------->
+     Bienvenido
+    <---------->
+    -> Seleccione una opción:
+    1.- Agregar Orden
+    2.- Mostrar Ordenes
+    3.- Eliminar Orden
+    4.- Actualizar Orden
+    5.- Salir
+    ''' )
+
 def add_order():
     cliente = input('Ingrese el nombre del cliente: ')
     sabor = input('Ingrese el sabor del pastel: ')
@@ -17,27 +32,25 @@ def add_order():
     return cliente,sabor,tamaño, color
 
 def update_order():
-    id_order = int(input("Ingrese el id del usuario que desea actualizar: "))
+    id_order = int(input('Ingrese el id del usuario que desea actualizar: '))
     cliente = input('Ingrese el nombre del cliente: ')
-    sabor = input("Ingrese el nuevo sabor del pastel: ")
-    tamaño = input("Ingrese el tamaño del pastel: ")
+    sabor = input('Ingrese el nuevo sabor del pastel: ')
+    tamaño = input('Ingrese el tamaño del pastel: ')
     color = input('Ingrese el nuevo color de betun del pastel: ')
 
     return cliente, sabor, tamaño, color, id_order
 
 
-print('''
-<---------->
-Bienvenido
-<---------->
+#def back_menu():
+#    back_decision = str(input('¿Desea realizar otra accion (S/N)? '))
+#    if back_decision == 'S' or 's':
+#        presentation()
+#        menu()
+#    elif back_decision == 'N' or 'n':
+#        exit
+#    else:
+#        print('Ingrese una opcion valida !!!')
 
-- Seleccione una opción:
-1.- Agregar Orden
-2.- Mostrar Ordenes
-3.- Eliminar Orden
-4.- Actualizar Orden
-5.- Salir
-''' )
 
 sql_add_order = "INSERT INTO pasteles (cliente,sabor,tamaño,color) VALUES(%s,%s,%s,%s)"
 sql_show = "SELECT * FROM pasteles"
@@ -45,21 +58,61 @@ sql_delete = "DELETE FROM pasteles WHERE id=%s"
 sql_update = "UPDATE pasteles SET cliente=%s sabor=%s tamaño=%s color=%s WHERE id=%s"
 
 def menu():
+
     connection = psycopg2.connect(
     host = 'localhost',
     user = 'nestorjr',
     password = '123456',
     dbname = 'pasteles')
-    print('<----- Conexion exitosa ----->')
 
     opcion = int(input('Ingrese el numero de la opcion seleccionada: '))
     with connection.cursor() as cursor:
         if opcion == 1:
             cursor.execute(sql_add_order, add_order())
             connection.commit()
-        if opcion == 2:
+            print('Orden Registrada con exito')
+            #back_menu()
+            
+        elif opcion == 2:
             cursor.execute(sql_show)
             for order in cursor.fetchall():
                 print(order)
+            #back_menu()
 
+        elif opcion == 3:
+            decision = str(input('Desea obtener el listado de Ordenes? (S/N): '))
+            if decision == 'S' or 's':
+                cursor.execute(sql_show)
+                for order in cursor.fetchall():
+                    print(order)
+                
+            elif decision == 'N' or 'n':
+                pass
+                
+            id_order = int(input('Ingrese el numero de orden a eliminar: '))   
+            cursor.execute(sql_delete,(id_order,))
+            connection.commit()
+            print('Orden eliminada con exito !!!')
+            #back_menu()
+            
+        elif opcion == 4:
+            cursor.execute(sql_show)
+            for order in cursor.fetchall():
+                print(order)
+            cursor.execute(sql_update, update_order())
+            connection.commit()
+            print('Orden actualizada con exito !!!')
+            #back_menu()
+        
+        elif opcion == 5:
+            exit
+        
+        else:
+            print('Ingrese una opcion valida !!!')
+            presentation()
+            menu()
+
+    connection.close()
+
+presentation()
 menu()
